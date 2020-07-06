e961a7b450496029d050c4ba121d28235a04d8eb
mprew97 <mitch.prewitt@getbellhops.com>
2020-07-05 22:22:05 -0400

    serve static files

---
+++ src/server/middleware/error.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import HttpException from '../common/http-exception';
+
+const errorHandler = (
+  error: HttpException,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const status = error.statusCode || 500;
+  const message = error.message || "It's not you. It's us. We are having some problems.";
+
+  response.status(status).send(message);
+};
+
+export default errorHandler;
